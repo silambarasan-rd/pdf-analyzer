98f868bc63d0dd0275f8e55345be2a5b93956cd9
Silambarasan R <silambu1821944@gmail.com>
2025-09-24 18:35:47 +0000

    Split into chunks and store it on ChromaDB

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_community.document_loaders import OnlinePDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
 
 local_path = "alchemist.pdf"
 
@@ -11,5 +14,15 @@ if local_path:
 else:
     print("Upload a local PDF file to proceed.")
 
-# Preview this data
-print(data[0].page_content[:500])
+# Split the text into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
+chunks = text_splitter.split_documents(data)
+
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embedding=OllamaEmbeddings(model="nomic-embed-text", show_progress=True),
+    collection_name="pdf-collection",
+)
+
+
+
